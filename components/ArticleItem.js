33ad64ee3bbf0704497d9f1424f1f6aaa5d38a97
Bruno Sacco <brusacco@gmail.com>
2021-11-20 13:27:54 -0300

    added nextjs link to articles card

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -28,30 +29,31 @@ const ArticleItem = ({ article, index, main = false }) => {
         title={article.site.name}
         subheader={drawDate(article.created_at)}
       />
+      <Link href={`/${article.slug}.html`} passHref>
+        <CardActionArea href={`/${article.slug}.html`}>
+          <Image
+            src={main ? article.image : article.image_mosaic}
+            alt={article.title}
+            width={article.image_size.width}
+            height={article.image_size.height}
+            layout='responsive'
+          />
 
-      <CardActionArea href={`/${article.slug}.html`}>
-        <Image
-          src={main ? article.image : article.image_mosaic}
-          alt={article.title}
-          width={article.image_size.width}
-          height={article.image_size.height}
-          layout='responsive'
-        />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div" color="text.primary">
+              {article.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {truncate(article.description, 200)}
+            </Typography>
 
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="div" color="text.primary">
-            {article.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {truncate(article.description, 200)}
-          </Typography>
+            <Stack direction="row" pt={5} spacing={1}>
+              {drawTags(article.tags)}
+            </Stack>
+          </CardContent>
 
-          <Stack direction="row" pt={5} spacing={1}>
-            {drawTags(article.tags)}
-          </Stack>
-
-        </CardContent>
-      </CardActionArea>
+        </CardActionArea>
+      </Link>
 
       <CardActions>
         <Share article={article} />
