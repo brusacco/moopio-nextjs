d2feb5b745500ab825ac96d605850e84ce98fff2
Bruno Sacco <brusacco@gmail.com>
2021-11-20 13:47:28 -0300

    static navbar

--- components/Nav.js
+++ components/Nav.js
@@ -9,34 +9,32 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ backgroundColor: 'black' }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href="/">
-              <a>
-                Moopio
-              </a>
-            </Link>
-          </Typography>
-          <Typography variant="h6" component="div">
-            <Link href="/subiendo">
-              <a>
-                <Button color="inherit">Populares</Button>
-              </a>
-            </Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky" style={{ backgroundColor: 'black' }}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link href="/">
+            <a>
+              Moopio
+            </a>
+          </Link>
+        </Typography>
+        <Typography variant="h6" component="div">
+          <Link href="/subiendo">
+            <a>
+              <Button color="inherit">Populares</Button>
+            </a>
+          </Link>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
